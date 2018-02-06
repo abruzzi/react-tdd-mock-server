60a623f09386bb2431288d153ee29f4ead0476e9
Juntao Qiu <juntao.qiu@gmail.com>
2018-02-06 14:41:11 +0800

    add a few middlewares for testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-
+const _ = require('lodash')
 
 function format(data, meta) {
 	const newData = Object.assign({}, {data}, {meta})
@@ -13,6 +13,10 @@ function format(data, meta) {
 }
 
 function parseQueryStringToJson(query) {
+  if(!query) {
+    return {}
+  }
+
 	const pairs = query.split('&')
 	return pairs.reduce((c, p) => {
 		const [key, value] = p.split('=')
@@ -21,20 +25,38 @@ function parseQueryStringToJson(query) {
 	}, {})
 }
 
-router.render = function (req, res) {
-  if (req.method === 'GET' && res.get('X-Total-Count')) {
-  	const query = url.parse(req.url).query
-  	const obj = parseQueryStringToJson(query)
+server.use((req, res, next) => {
+  const query = url.parse(req.url).query
+  req.query = parseQueryStringToJson(query)
+  next()
+})
 
-    const newData = format(res.locals.data, {total: res.get('X-Total-Count'), page: parseInt(obj['_page']), size: parseInt(obj['_limit'])})
-    res.json(newData)
+server.use((req, res, next) => {
+  const parts = req.path.split('/')
+
+  if(!_.isNaN(_.toNumber(_.last(parts)))) {
+    req.entity = _.nth(parts, -2)
   } else {
-    res.json(res.locals.data)
+    req.entity = _.last(parts)
   }
-}
+
+  next()
+})
+
+
+server.use((req, res, next) => {
+  if(req.method === 'DELETE' && req.query['_cleanup']) {
+    const db = router.db
+    db.set(req.entity, []).write()
+    res.sendStatus(204)
+  } else {
+    next()  
+  }
+})
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
+
 
 server.use(router)
 server.listen(8080, () => {
